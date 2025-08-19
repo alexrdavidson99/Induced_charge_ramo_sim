5485b59113724d10c71648442bf955d2132bd7a9
alexrdavidson99 <alex.davidson@warwick.ac.uk>
2025-08-19 15:41:26 +0100

    updated with 1500v results

--- plots.py
+++ plots.py
@@ -3,6 +3,46 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import h5py
+import functools
+from pathlib import Path
+import re
+from scipy.optimize import curve_fit
+from scipy.signal import chirp, find_peaks, peak_widths
+
+# import mplhep
+# mplhep.style.use(mplhep.style.LHCb2)
+# from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
+from scipy.special import erf
+#Gaussian function
+def gaus(x, a, x0, sigma):
+    return a * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2)) 
+
+def gaussian_convolved_tophat(x, amp, mean, stddev, width=0.55):
+    return amp * (erf((x - mean + width / 2) / (np.sqrt(2) * stddev)) - erf((x - mean - width / 2) / (np.sqrt(2) * stddev))) / 2
+
+
+def load_and_group_sum(DATA_DIR,shift, round_decimals=4):
+    
+    print(f"Data directory: {DATA_DIR}")
+    # Get files for each pattern
+    files = list(DATA_DIR.glob(f"induced_charge_tom_P*off*{shift}*.csv"))
+    print(f"Files found: {files}")
+    df = pd.concat([pd.read_csv(file) for file in files], ignore_index=True)
+    df['Time'] = df['Time'].round(round_decimals)
+    df.groupby('Time', as_index=False)['Current'].sum()
+    #df.to_csv(DATA_DIR / f'summed_induced_current_from_tom_p_center__off_in_x.csv', index=False)
+    return df.groupby('Time', as_index=False)['Current'].sum()
+
+def extract_z_value(s):
+    match = re.search(r"z_(-?\d+)", s)
+    if match:
+        return float(match.group(1))
+    match = re.search(r"z_neg_(\d+)", s)
+    if match:
+        return -float(match.group(1))
+    return 0.0
+
 
 
 def plot_field(data, z_position):
@@ -32,20 +72,20 @@ def plot_current(filename):
     #plt.ylim(-1e-5, 1e-6)
     
     
-filenames = [
-    'C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/induced_charge_10k_off_tom_pix.csv',
-    'C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/induced_charge_10k_on_tom_pix.csv',
-]
-plt.figure()
-for filename in filenames:
-    plot_current(filename)
+# filenames = [
+#     'C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/induced_charge_10k_off_tom_pix.csv',
+#     'C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/induced_charge_10k_on_tom_pix.csv',
+# ]
+# plt.figure()
+# for filename in filenames:
+#     plot_current(filename)
     
-plt.xlabel('Time (ns)')
-plt.ylabel('Induced Current (A)')
+# plt.xlabel('Time (ns)')
+# plt.ylabel('Induced Current (A)')
 
-plt.legend()
-plt.title('Induced Current Over Time')
-plt.show()
+# plt.legend()
+# plt.title('Induced Current Over Time')
+# plt.show()
 
 #file_path = './3d/E-Field [Es].h5'
 #z_value = -16  # Set your desired z index or value
@@ -79,5 +119,158 @@ plt.show()
 # plt.title(f'|E| at z={z[z_idx]:.2f}')
 # plt.colorbar(label='|E|')
 # plt.show()
+
+#load charge tom_ p csv data for all electrons from 1 to 1309561
+# concat all csv files into one dataframe
+
+Path_to_induced_charge = 'C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/induced_current/1000V/'
+
+
+DATA_DIR = Path(Path_to_induced_charge)
+print(f"Data directory: {DATA_DIR}")
+files = DATA_DIR.glob(f"induced_charge_tom_P*off*z*.csv")
+for file in files:
+    print(f"Processing file: {file}")
+    df = pd.read_csv(file)
+    # Plot the current data
+    plt.plot(df['Time'], df['Current'], label=file.name)
+
+
+# Path_to_induced_charge = 'C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/'
+
+
+# DATA_DIR = Path(Path_to_induced_charge)
+# print(f"Data directory: {DATA_DIR}")
+# files = DATA_DIR.glob(f"induced_charge_tom_P*off*z*.csv")
+# for file in files:
+#     print(f"Processing file: {file}")
+#     df = pd.read_csv(file)
+#     # Plot the current data
+#     plt.plot(df['Time'], df['Current'], label=file.name)
+
+
+plt.figure(figsize=(10, 6))
+DATA_DIR = Path(Path_to_induced_charge)
+print(f"Data directory: {DATA_DIR}")
+
+
+
+
+#files_set2 = list(DATA_DIR.glob("induced_charge_tom_P*off*z*.csv"))
+#z_50'
+DATA_DIR_v = Path('C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/induced_current/1500V/')
+#DATA_DIR_v = Path('C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/induced_current/') # no file = 1000V
+#DATA_DIR_v = Path('C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/induced_current/')
+#list_of_positions = ['x', 'z_49.9','z_60', 'z_100','z_175','z_550', 'z_225', 'z_275', 'z_300', 'z_400', 
+#                     'z_350', 'z_450', 'z_500', 'z_1100', 'z_1650', 'z_neg_60','z_neg_1040', 'z_neg_1590', 'z_neg_550','z_neg_410', 'z_neg_275','z_neg_100','z_neg_150','z_neg_200']
+                     #'z_neg_100']
+#list_of_positions = ['z_60','z_550', 'z_400','z_225', 'z_neg_550', 'z_neg_275', 'z_neg_100']
+#list_of_positions = ['z_60','z_350','z_550','z_1100']
+#list_of_positions = ['z_60','85','z_490','z_neg_490']
+list_of_positions = ['z_neg_490','z_neg_50', 'z_neg_150','z_neg_250','z_0','z_60','z_75','z_85','z_150','z_250','z_325','z_375','z_490','z_610']
+#list_of_positions = ['x','z_60','z_550', 'z_1100', 'z_1650', 'z_neg_60']
+                     #'z_neg_100']
+#list_of_positions = ['z_60']
+
+
+peaks_array = []
+peaks_positions = []
+
+for position in list_of_positions:
+    grouped = load_and_group_sum(DATA_DIR_v,position)
+    print(f"Processing group sum : {position }")
+    max_current = grouped['Current'].min()
+    peaks_array.append(max_current)
+    print(f"Max current for {position}: {max_current}")
+    z_co = extract_z_value(position)
+    peaks_positions.append(z_co)
+    plt.plot(z_co, max_current, 'o', label=position)
+    #plt.plot(grouped['Time'], grouped['Current']*1e3, label= position)
     
+    grouped.to_csv(DATA_DIR_v / f'summed_induced_current_from_tom_p_center__off_in_x_test.csv', index=False)
+
+
+plt.xlabel('Time (ns)')
+plt.ylabel('Peak current (mA) test')
+plt.title(' Simulated Induced Current at Different Positions')
+#plt.yscale('log')
+plt.legend()
+plt.xlim(-1500, 1500)
+
+#peaks v  peak  posotions  datafame 
+peaks_df = pd.DataFrame({'Position': peaks_positions, 'Peak Current (A)': peaks_array})
+DATA_DIR_P = Path('C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/')
+peaks_df.to_csv(DATA_DIR_P / 'peaks_positions_1000V.csv', index=False)
+
+# popt, pcov = curve_fit(gaussian_convolved_tophat, peaks_positions, peaks_array, p0=[0.0008, 60, 100])
+# peaks_positions_array = np.array(peaks_positions)
+# peaks_array = np.array(peaks_array)
+# plt.figure(figsize=(10, 6))
+# x_fit = np.linspace(min(peaks_positions), max(peaks_positions), 1000)
+# plt.plot(x_fit, gaussian_convolved_tophat(x_fit, *popt)*1e3, 'r--', label=f'Gaussian Fit FWHM = {2 * np.sqrt(2 * np.log(2)) * popt[2]:.0f}um')
+# plt.scatter(peaks_positions_array, peaks_array*1e3,  label='Data from simulation')
+# print(f"FWHM: {2 * np.sqrt(2 * np.log(2)) * popt[2]}")  # Full Width at Half Maximum
+# plt.title('Peak Current vs Position')
+
+
+
+# plt.xlabel('z position (um)')
+
+# plt.ylabel('peak current (mA)')
+
+plt.legend()
+# plt.title('Different Patterns of Induced Current')
+
+
+
+# signal = -grouped1['Current'].values
+# time = grouped1['Time'].values
+
+# # Normalize the signal between 0 and 1
+# signal_norm = (signal - np.min(signal)) / (np.max(signal) - np.min(signal))
+
+# # Find indices where signal crosses 10% and 90%
+# idx_10 = np.where(signal_norm >= 0.1)[0][0]
+# idx_90 = np.where(signal_norm >= 0.9)[0][0]
+
+# t_10 = time[idx_10]
+# t_90 = time[idx_90]
+# rise_time = t_90 - t_10
+
+# print(f"10% rise time: {t_10}")
+# print(f"90% rise time: {t_90}")
+# print(f"Rise time (10% to 90%): {rise_time}")
+# plt.vlines([t_10,t_90], ymin=0, ymax=-0.0008, colors='red', linestyles='dashed', label='Rise Time')
+# plt.legend()
+
+# V1500 = pd.read_csv("C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/induced_current/1500V/summed_induced_current_from_tom_p_center__off_in_x_test.csv")
+# V1000 = pd.read_csv("C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/induced_current/summed_induced_current_from_tom_p_center__off_in_x_test.csv")
+# plt.figure(figsize=(10, 6))
+# plt.plot(V1500['Time'], V1500['Current']*1e3, label='1500V', color='blue')
+# plt.plot(V1000['Time'], V1000['Current']*1e3, label='1000V', color='orange')
+# plt.xlabel('Time (ns)')
+# plt.ylabel('Induced Current (mA)')
+# plt.title('Induced Current at 1500V and 1000V')
+
+V1500 = pd.read_csv("C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/peaks_positions_1500V.csv")
+V1000 = pd.read_csv("C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/peaks_positions_1000V.csv")
+plt.figure(figsize=(10, 6))
+plt.scatter(V1500['Position'], V1500['Peak Current (A)'], label='1500V', color='blue')
+plt.scatter(V1000['Position'], V1000['Peak Current (A)'], label='1000V', color='orange')
+plt.xlabel('Position (um)')
+plt.ylabel('Induced Current (mA)')
+plt.title('Induced Current at 1500V and 1000V')
+
+
+V1500 = pd.read_csv("C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/x_z_data_from_python_1500v.csv")
+V1000 = pd.read_csv("C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/x_z_data_from_python_1.3_million.csv")
+plt.figure(figsize=(10, 6)) 
+plt.scatter(V1000['x_position'], V1000['z_position'], label='1000V', color='orange')
+plt.scatter(V1500['x_position'], V1500['z_position'], label='1500V', color='blue')
+
+plt.xlabel('Position (um)')
+plt.ylabel('Position (um)')
+plt.title('Hit map at 1500V and 1000V')
+
+plt.show()
 
--- ramo_sim.py
+++ ramo_sim.py
@@ -24,8 +24,8 @@ def find_closest_values(target_value, x, y, z):
     return closest_value_x, closest_value_y, closest_value_z
 
 c = scipy.constants.speed_of_light * 1e-3  # in um/ns
-V = 1000  # electrods potential in V
-d = 2500 # mcp anode gap in um
+V = 1500  # electrods potential in V
+d = 2500 #2489.798052 # mcp anode gap in um
 m = 511e3
 E = V * (c ** 2) / (d * m)  # electric field acceleration in um/ns^2
 E_field = V/(d*1e-6) 
@@ -52,178 +52,162 @@ with h5py.File(file_path, 'r') as f:
     x = f['Mesh line x'][:]
     y = f['Mesh line y'][:]
     z = f['Mesh line z'][:]
+    # Load the entire dataset into memory
+    E_Field = f['E-Field'][:]  
 
-sum_data = []
-position_start_data_x = []
-position_start_data_z = []
+
+# position_start_data_x = []
+# position_start_data_z = []
 position_end_data_x = []
 position_end_data_z = []
 
-electron_x = []
-electron_y = []
-electron_z = []
+# electron_x = []
+# electron_y = []
+# electron_z = []
 
-energies = []
-alpha_list = []
-vabs_list = []
-shift_a_pad = 0
+#energies = []
+#alpha_list = []
+#vabs_list = []
+shift_a_pad = 75
 #print(len(start_position_data["Velocity [X]"]))
 num_electrons = len(start_position_data)
-middle = num_electrons // 2
-indices = list(range(5)) + list(range(num_electrons - 5, num_electrons))
+print(f"Number of electrons = {num_electrons}")
+#middle = num_electrons // 2
+#indices = list(range(5)) + list(range(num_electrons - 5, num_electrons))
 #indices = list(range(middle - 1, middle + 1))
-indices = range(200000)  # Exclude the first and last 5 indices
-for j in indices:
+indices = range(1) #range(num_electrons)
+batch_size = 100000
+total_range = num_electrons
 
-    index_electron = j
-    target_coord = np.array([start_position_data["Position [X]"][index_electron]+ shift_a_pad,
-                             start_position_data["Position [Y]"][index_electron],
-                             start_position_data["Position [Z]"][index_electron] , ]) #
-    position_start_data_x.append(target_coord[0])
-    position_start_data_z.append(target_coord[2])
+for batch_start in range(0, total_range, batch_size):
+    sum_data = []
+    for j in range(batch_start, min(batch_start + batch_size, total_range)):
+        index_electron = j
+        target_coord = np.array([start_position_data["Position [X]"][index_electron],
+                                start_position_data["Position [Y]"][index_electron],
+                                start_position_data["Position [Z]"][index_electron] + shift_a_pad]) #
+        #position_start_data_x.append(target_coord[0])
+        #position_start_data_z.append(target_coord[2])
 
 
-    v0 = np.array([start_position_data["Velocity [X]"][index_electron],
-                   start_position_data["Velocity [Y]"][index_electron],
-                   start_position_data["Velocity [Z]"][index_electron]]) # [m/s]
-    
-    vabs = np.sqrt(v0[0] ** 2 + v0[1] ** 2 + v0[2] ** 2)
-    
-    energy = ( vabs / scipy.constants.speed_of_light) ** 2 * 0.5 * m
-    #print(f"Energy = {energy} eV")
-    vabs_list.append(vabs)
-    alpha = np.arctan(v0[1] / v0[0])
-    alpha_list.append(alpha)
+        v0 = np.array([start_position_data["Velocity [X]"][index_electron],
+                    start_position_data["Velocity [Y]"][index_electron],
+                    start_position_data["Velocity [Z]"][index_electron]]) # [m/s]
 
-    energies.append(energy)
-    #print(f"start_position = {target_coord}")
-    closest_x, closest_y, closest_z = find_closest_values(target_coord, x, y, z)
-    # print(f"Closest value to {target_coord[0]} in x: {x[closest_x]}")
-    # print(f"Closest value to {target_coord[1]} in y: {y[closest_y]}")
-    # print(f"Closest value to {target_coord[2]} in z: {z[closest_z]}")
+        vabs = np.sqrt(v0[0] ** 2 + v0[1] ** 2 + v0[2] ** 2)
 
-    with h5py.File(file_path, 'r') as f:
-        field_array = f['E-Field'][closest_z, closest_y, closest_x]
-    field_x = field_array['x']
-    field_y = field_array['y']
-    field_z = field_array['z']
-
-    # Create a numerical vector for the electric field
-    field_vector = np.array([field_x, field_y, field_z])
-    field_array = field_vector
-    #print (f"field_array = {field_array*-1.6e-19}")
-
-    current = -1.6e-19*np.dot(v0, field_array) #[m/s]  * [V/m] = [A]
-    #print(f"Current = {current} A")
-
-    x0 = target_coord
-
-    v0 = v0*1e-3
-    t = solve_for_intercept_time(x0, v0, a0, target_coord[1]+d) #d is distance to the anode in um
-
-    #print(f"t = {t} time in ns")
-    #print (f"start velocity = {v0} um/ns")
-    #print (f"position_end = {step_position(x0, v0, a0, t)} um")
-    position_end = step_position(x0, v0, a0, t)
-    position_end_data_x.append(position_end[0])
-    position_end_data_z.append(position_end[2])
-    t_max = 5 # [ns]
-    step = 1000 # number of steps to take in the time interval
-    induced_current = []
-    field_array_steps_y = []
-    start_time_of_electron = np.array(start_position_data["Time"][index_electron]) * 1e9
-    #print(f"start_time_of_electron = {start_time_of_electron}")
-
-    for i in range (step):
-        time_step = 0.001 * i
-        if time_step >= t:
-            #print(f"time step stopped at  = {time_step} ns")
-            sum_data.append({"Time": time_step + start_time_of_electron, "Current": 0})
-            #for k in range(10):
-            #    sum_data.append({"Time": time_step + start_time_of_electron + (k/10)*2, "Current": 0})
-            break
-
-        xi = step_position(x0, v0, a0, time_step)
-        vi = step_velocity(v0, a0, time_step)
-        
-        #xi = np.array([5.2, 3507, -30])
-        #xi = np.array([-4985.1, 1515, -2010])
-        #xi = np.array([-1379.8, 1515, -2010])
-        closest_x, closest_y, closest_z = find_closest_values(xi, x, y, z)
-        
-
-        with h5py.File(file_path, 'r') as f:
-            #field_array = f['E-Field'][closest_x, closest_y, closest_z]
-            field_array = f['E-Field'][closest_z, closest_y, closest_x]
-   
-        # print(f"Closest value to {xi[0]} in x: {x[closest_x]}")
-        # print(f"Closest value to {xi[1]} in y: {y[closest_y]}")
-        # print(f"Closest value to {xi[2]} in z: {z[closest_z]}")
-        field_array = np.array(field_array)
-        field_x = field_array['x']
-        field_y = field_array['y']
-        
-        field_z = field_array['z']
-
-        # Create a numerical vector for the electric field
-        field_vector = np.array([field_x, field_y, field_z])
-        field_array = field_vector
-        field_array_steps_y.append(field_array[1])
-        #print (f"field_array = {field_array}")
-
-        #energy = ((np.sqrt(vi[0] ** 2 + vi[1] ** 2 + vi[2] ** 2)) / c) ** 2 * 0.5 * m
+        energy = ( vabs / scipy.constants.speed_of_light) ** 2 * 0.5 * m
         #print(f"Energy = {energy} eV")
-        current = 1.6e-19 * np.dot(vi*1e3, field_array)
-        # print(f"field_array = {field_array}")
-        # print(f"velocity = {vi*1e3} m/s")
-        induced_current.append(current)
+        #vabs_list.append(vabs)
+        alpha = np.arctan(v0[1] / v0[0])
+        #alpha_list.append(alpha)
+
+        #energies.append(energy)
+        #print(f"start_position = {target_coord}")
+
+        closest_x, closest_y, closest_z = find_closest_values(target_coord, x, y, z)
+        field_array = E_Field[closest_z, closest_y, closest_x]
+        field_vector = np.array([field_array['x'], field_array['y'], field_array['z']])
+
+
+        current = -1.6e-19*np.dot(v0, field_vector) #[m/s]  * [V/m] = [A]
         #print(f"Current = {current} A")
 
+        x0 = target_coord
 
-        sum_data.append({"Time": time_step+start_time_of_electron, "Current": current})
-        electron_x.append(xi[0])
-        electron_y.append(xi[1])
-        electron_z.append(xi[2])
+        v0 = v0*1e-3
+        t = solve_for_intercept_time(x0, v0, a0, target_coord[1]+d) #d is distance to the anode in um
 
-        #print(f"Energy = {energy} eV")
+        #print(f"t = {t} time in ns")
+        #print (f"start velocity = {v0} um/ns")
+        #print (f"position_end = {step_position(x0, v0, a0, t)} um")
+        position_end = step_position(x0, v0, a0, t)
+        position_end_data_x.append(position_end[0])
+        position_end_data_z.append(position_end[2])
+        t_max = 5 # [ns]
+        step = 1000 # number of steps to take in the time interval
+        induced_current = []
+        field_array_steps_y = []
+        start_time_of_electron = np.array(start_position_data["Time"][index_electron]) * 1e9
+        #print(f"start_time_of_electron = {start_time_of_electron}")
 
-        #print(f"x = {xi}")
+        for i in range (step):
+            time_step = 0.001 * i   # # time step in ns normally 0.001
+            if time_step >= t:
+                #print(f"time step stopped at  = {time_step} ns")
+                sum_data.append({"Time": time_step + start_time_of_electron, "Current": 0})
+                #for k in range(10):
+                #    sum_data.append({"Time": time_step + start_time_of_electron + (k/10)*2, "Current": 0})
+                break
 
-df = pd.DataFrame(sum_data)
-zero_index = df[df['Current'] == 0].index[0]
-plt.scatter(df["Time"][:zero_index+1], df["Current"][:zero_index+1])
-plt.scatter(df["Time"][zero_index+1:], df["Current"][zero_index+1:])
-#plt.plot(df["Time"][:-1], field_array_steps_y)
-plt.xlabel("Time (ns)")
-plt.ylabel("Current (A)")
-plt.title("Induced Current")
-result_sort = df.sort_values(["Time"], ascending=[True])
-last_time = result_sort['Time'].iloc[-1]
+            xi = step_position(x0, v0, a0, time_step)
+            vi = step_velocity(v0, a0, time_step)
 
-#Generate additional zero current entries
-additional_entries = [{'Time': last_time + 0.001 * i, 'Current': 0} for i in range(1, 501)]
+            #xi = np.array([5.2, 3507, -30])
+            #xi = np.array([-4985.1, 1515, -2010])
+            #xi = np.array([-1379.8, 1515, -2010])
 
-# Append to result_sort
-additional_df = pd.DataFrame(additional_entries)
-result_sort = pd.concat([result_sort, additional_df], ignore_index=True)
+            # #energy = ((np.sqrt(vi[0] ** 2 + vi[1] ** 2 + vi[2] ** 2)) / c) ** 2 * 0.5 * m
+            # #print(f"Energy = {energy} eV")
+            closest_x, closest_y, closest_z = find_closest_values(xi, x, y, z)
+            #  print(f"Closest value to {xi[0]} in x: {x[closest_x]}")
+            # print(f"Closest value to {xi[1]} in y: {y[closest_y]}")
+            # print(f"Closest value to {xi[2]} in z: {z[closest_z]}")
+            field_array = E_Field[closest_z, closest_y, closest_x]
+            # # Create a numerical vector for the electric field
+            field_vector = np.array([field_array['x'], field_array['y'], field_array['z']])
 
-df['Time'] = df['Time'].round(4) 
-grouped_sum = df.groupby('Time', as_index=False)['Current'].sum()
+            current = 1.6e-19 * np.dot(vi*1e3, field_vector)
+            # print(f"field_array = {field_array}")
+            # print(f"velocity = {vi*1e3} m/s")
+            induced_current.append(current)
+            #print(f"Current = {current} A")
 
-print(grouped_sum[grouped_sum.isna().any(axis=1)])
-# Optional: sort by Time if not already sorted
-#grouped_sum = grouped_sum.sort_values(by='Time')
 
-print("Rows with zero Current:")
-print(grouped_sum[grouped_sum['Current'] == 0])
+            sum_data.append({"Time": time_step+start_time_of_electron, "Current": current})
+    #electron_x.append(xi[0])
+#electron_y.append(xi[1])
+#electron_z.append(xi[2])
 
-# Plotting
-plt.figure()
-plt.plot(grouped_sum['Time'], grouped_sum['Current'])
-plt.xlabel("Time (ns)")
-plt.ylabel("Current (A)")
-plt.title("Induced Charge")
-grouped_sum.to_csv('C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/induced_charge.csv', index=False)
+#print(f"Energy = {energy} eV")
+
+#print(f"x = {xi}")
+
+    df = pd.DataFrame(sum_data)
+    zero_index = df[df['Current'] == 0].index[0]
+    plt.scatter(df["Time"][:zero_index+1], df["Current"][:zero_index+1])
+    plt.scatter(df["Time"][zero_index+1:], df["Current"][zero_index+1:])
+    #plt.plot(df["Time"][:-1], field_array_steps_y)
+    plt.xlabel("Time (ns)")
+    plt.ylabel("Current (A)")
+    plt.title("Induced Current")
+    result_sort = df.sort_values(["Time"], ascending=[True])
+    last_time = result_sort['Time'].iloc[-1]
+
+    #Generate additional zero current entries
+    additional_entries = [{'Time': last_time + 0.001 * i, 'Current': 0} for i in range(1, 501)]
+
+    # Append to result_sort
+    additional_df = pd.DataFrame(additional_entries)
+    result_sort = pd.concat([result_sort, additional_df], ignore_index=True)
+
+    df['Time'] = df['Time'].round(4) 
+    grouped_sum = df.groupby('Time', as_index=False)['Current'].sum()
+
+    print(grouped_sum[grouped_sum.isna().any(axis=1)])
+    # Optional: sort by Time if not already sorted
+    #grouped_sum = grouped_sum.sort_values(by='Time')
+
+    print("Rows with zero Current:")
+    print(grouped_sum[grouped_sum['Current'] == 0])
+
+    # Plotting
+    plt.figure()
+    plt.plot(grouped_sum['Time'], grouped_sum['Current'])
+    plt.xlabel("Time (ns)")
+    plt.ylabel("Current (A)")
+    plt.title("Induced Charge")
+    grouped_sum.to_csv(f'C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/induced_charge_tom_P_1500v_fix_{j-batch_size}_{j}_off_center_z_75.csv', index=False)
 
 print("Rows where Time = 0.06:")
 print(grouped_sum[grouped_sum['Time'] == 0.06])
@@ -238,38 +222,43 @@ print(grouped_sum[grouped_sum['Time'] == 0.06])
 # plt.ylabel("Current (A)")
 # plt.title("Induced Charge")
 # plt.xlim(0, 10)
-plt.figure()
-plt.scatter(position_end_data_x, position_end_data_z, color='b',label='python',alpha=0.5)
-plt.figure()
-plt.hist2d(position_end_data_x, position_end_data_z, cmap='plasma', bins=50)
-plt.figure()
-box_coords = [(-143, -143), (-143, 143), (143, 143), (143, -143)]
-polygon = Polygon(box_coords, closed=True, edgecolor='g', linestyle='--', fill=None)
-plt.gca().add_patch(polygon)
-data_from_csv = pd.read_csv('C:/Users/lexda/Downloads/compare_CST_with_python_CST_phase_space.txt',
-                            names=["X","Z"], sep="\t+", comment='#')
-plt.scatter(data_from_csv["X"], data_from_csv["Z"], color='r',label='CST',alpha=0.5)
+# plt.figure()
+# plt.scatter(position_end_data_x, position_end_data_z, color='b',label='python',alpha=0.5)
+# x_z_data = {"x_position": position_end_data_x,
+#             "z_position": position_end_data_z }
+# df_x_z = pd.DataFrame(x_z_data)
+# df_x_z.to_csv(f'C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/x_z_data_from_python_1500v.csv', index=False)
 
-plt.legend()
-plt.xlabel('x[um]')
-plt.ylabel('z[um]')
-plt.title('Comparison between CST and python')
-plt.figure()
-plt.hist(data_from_csv["X"], bins=100)
-plt.hist(data_from_csv["Z"], bins=100)
-plt.xlabel('x[um]')
-plt.figure()
-plt.hist2d(alpha_list, vabs_list, cmap='plasma', bins=50)
-plt.colorbar()
-plt.xlabel('alpha[rad]')
-plt.ylabel('vabs[m/s]')
-figure()
-alpha_list = [abs(alpha) for alpha in alpha_list]
 
-#print (alpha_list)
-alpha_list = np.array(alpha_list)
-plt.hist(90-alpha_list*(180/np.pi), bins=100)
-plt.xlabel('Energy[eV]')
+# plt.figure()
+# plt.hist2d(position_end_data_x, position_end_data_z, cmap='plasma', bins=50)
+# plt.figure()
+# box_coords = [(-1650, -275), (-1650, 275), (1650, 275), (1650, -275)]
+# polygon = Polygon(box_coords, closed=True, edgecolor='g', linestyle='--', fill=None)
+# plt.gca().add_patch(polygon)
+# data_from_csv = pd.read_csv('C:/Users/lexda/PycharmProjects/Induced_charge_ramo_sim/2d_x_z_positins_-1.3_million_from_CST_1000V', sep=",")
+# plt.scatter(data_from_csv["x_position"], data_from_csv["z_position"], color='r',label='CST',alpha=0.5)
+
+#plt.legend()
+# plt.xlabel('x[um]')
+# plt.ylabel('z[um]')
+# plt.title('Comparison between CST and python')
+# plt.figure()
+# plt.hist(data_from_csv["X"], bins=100)
+# plt.hist(data_from_csv["Z"], bins=100)
+# plt.xlabel('x[um]')
+# plt.figure()
+# plt.hist2d(alpha_list, vabs_list, cmap='plasma', bins=50)
+# plt.colorbar()
+# plt.xlabel('alpha[rad]')
+# plt.ylabel('vabs[m/s]')
+# figure()
+# alpha_list = [abs(alpha) for alpha in alpha_list]
+
+# #print (alpha_list)
+# alpha_list = np.array(alpha_list)
+# plt.hist(90-alpha_list*(180/np.pi), bins=100)
+# plt.xlabel('Energy[eV]')
 
 
 # fig = plt.figure()
